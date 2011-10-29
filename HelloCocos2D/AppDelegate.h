9929e524fc0fd53ee99c3efdb0efb2ba9c420dbb
EdisonWang <jawwang1129@gmail.com>
2011-10-30 01:32:49 +0800

    Initial Commit

---
+++ HelloCocos2D/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  HelloCocos2D
+//
+//  Created by Edison on 10/30/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ HelloCocos2D/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloCocos2D
+//
+//  Created by Edison on 10/30/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
