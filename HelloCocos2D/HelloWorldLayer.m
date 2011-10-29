54c7d6a29f198c43056d915b42308a98b15087fd
EdisonWang <jawwang1129@gmail.com>
2011-10-30 01:51:50 +0800

    implement simple collision detection

--- HelloCocos2D/HelloWorldLayer.m
+++ HelloCocos2D/HelloWorldLayer.m
@@ -13,6 +13,9 @@
 // HelloWorldLayer implementation
 @implementation HelloWorldLayer
 
+NSMutableArray *_targets;
+NSMutableArray *_projectiles;
+
 +(CCScene *) scene
 {
 	// 'scene' is an autorelease object.
@@ -31,6 +34,9 @@
 // on "init" you need to initialize your instance
 -(id) init
 {
+    _targets = [[NSMutableArray alloc] init];
+    _projectiles = [[NSMutableArray alloc] init];
+    
     if( (self=[super initWithColor:ccc4(255, 255, 255, 255)] )) {
         CGSize winSize = [[CCDirector sharedDirector] winSize];
         CCSprite *player = [CCSprite spriteWithFile:@"Player.png" 
@@ -40,7 +46,8 @@
     }
     
     [self schedule:@selector(gameLogic:) interval:1.0];
-    
+    [self schedule:@selector(update:)];
+    self.isTouchEnabled = YES;
     return self;
 }
 
@@ -74,15 +81,108 @@
                                              selector:@selector(spriteMoveFinished:)];
     [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
     
+    target.tag = 1;
+    [_targets addObject:target];
 }
 
 -(void)spriteMoveFinished:(id)sender {
     CCSprite *sprite = (CCSprite *)sender;
+    if (sprite.tag == 1) { // target
+        [_targets removeObject:sprite];
+    } else if (sprite.tag == 2) { // projectile
+        [_projectiles removeObject:sprite];
+    }
     [self removeChild:sprite cleanup:YES];
 }
 
 -(void)gameLogic:(ccTime)dt {
     [self addTarget];
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:[touch view]];
+    location = [[CCDirector sharedDirector] convertToGL:location];
+    
+    // Set up initial location of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"Projectile.png" 
+                                               rect:CGRectMake(0, 0, 20, 20)];
+    projectile.position = ccp(20, winSize.height/2);
+    
+    // Determine offset of location to projectile
+    int offX = location.x - projectile.position.x;
+    int offY = location.y - projectile.position.y;
+    
+    // Bail out if we are shooting down or backwards
+    if (offX <= 0) return;
+    
+    // Ok to add now - we've double checked position
+    [self addChild:projectile];
+    
+    // Determine where we wish to shoot the projectile to
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offY / (float) offX;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+    
+    // Determine the length of how far we're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
+    float velocity = 480/1; // 480pixels/1sec
+    float realMoveDuration = length/velocity;
+    
+    // Move projectile to actual endpoint
+    [projectile runAction:[CCSequence actions:
+                           [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+                           [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
+                           nil]];
+    projectile.tag = 2;
+    [_projectiles addObject:projectile];
+}
+
+- (void)update:(ccTime)dt {
+    
+    NSMutableArray *projectilesToDelete = [[NSMutableArray alloc] init];
+    for (CCSprite *projectile in _projectiles) {
+        CGRect projectileRect = CGRectMake(
+                                           projectile.position.x - (projectile.contentSize.width/2), 
+                                           projectile.position.y - (projectile.contentSize.height/2), 
+                                           projectile.contentSize.width, 
+                                           projectile.contentSize.height);
+        
+        NSMutableArray *targetsToDelete = [[NSMutableArray alloc] init];
+        for (CCSprite *target in _targets) {
+            CGRect targetRect = CGRectMake(
+                                           target.position.x - (target.contentSize.width/2), 
+                                           target.position.y - (target.contentSize.height/2), 
+                                           target.contentSize.width, 
+                                           target.contentSize.height);
+            
+            if (CGRectIntersectsRect(projectileRect, targetRect)) {
+                [targetsToDelete addObject:target]; 
+            } 
+        }
+        
+        for (CCSprite *target in targetsToDelete) {
+            [_targets removeObject:target];
+            [self removeChild:target cleanup:YES]; 
+        }
+        
+        if (targetsToDelete.count > 0) {
+            [projectilesToDelete addObject:projectile];
+        }
+        [targetsToDelete release];
+    }
+    
+    for (CCSprite *projectile in projectilesToDelete) {
+        [_projectiles removeObject:projectile];
+        [self removeChild:projectile cleanup:YES];
+    }
+    [projectilesToDelete release];
 }
 
 // on "dealloc" you need to release all your retained objects
@@ -92,6 +192,11 @@
 	// in this particular example nothing needs to be released.
 	// cocos2d will automatically release all the children (Label)
 	
+    [_targets release];
+    _targets = nil;
+    [_projectiles release];
+    _projectiles = nil;
+    
 	// don't forget to call "super dealloc"
 	[super dealloc];
 }
