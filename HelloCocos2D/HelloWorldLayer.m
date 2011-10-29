d9330f422e319c0581b35cfc407c35808a575986
EdisonWang <jawwang1129@gmail.com>
2011-10-30 01:41:42 +0800

    add target

--- HelloCocos2D/HelloWorldLayer.m
+++ HelloCocos2D/HelloWorldLayer.m
@@ -38,7 +38,51 @@
         player.position = ccp(player.contentSize.width/2, winSize.height/2);
         [self addChild:player]; 
     }
+    
+    [self schedule:@selector(gameLogic:) interval:1.0];
+    
     return self;
+}
+
+-(void)addTarget {
+    
+    CCSprite *target = [CCSprite spriteWithFile:@"Target.png" 
+                                           rect:CGRectMake(0, 0, 27, 40)]; 
+    
+    // Determine where to spawn the target along the Y axis
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    int minY = target.contentSize.height/2;
+    int maxY = winSize.height - target.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the target slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    target.position = ccp(winSize.width + (target.contentSize.width/2), actualY);
+    [self addChild:target];
+    
+    // Determine speed of the target
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    id actionMove = [CCMoveTo actionWithDuration:actualDuration 
+                                        position:ccp(-target.contentSize.width/2, actualY)];
+    id actionMoveDone = [CCCallFuncN actionWithTarget:self 
+                                             selector:@selector(spriteMoveFinished:)];
+    [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
+-(void)spriteMoveFinished:(id)sender {
+    CCSprite *sprite = (CCSprite *)sender;
+    [self removeChild:sprite cleanup:YES];
+}
+
+-(void)gameLogic:(ccTime)dt {
+    [self addTarget];
 }
 
 // on "dealloc" you need to release all your retained objects
